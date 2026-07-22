4525bb82f1a577cde7f3de590efd2ebe2153c60b
Pecorine <73974565+zhankyou@users.noreply.github.com>
2026-07-22 09:00:22 -0500

    Update db_config.py

--- modulos/db_config.py
+++ modulos/db_config.py
@@ -7,11 +7,11 @@ load_dotenv()
 os.environ["PGCLIENTENCODING"] = "utf-8"
 
 def verificar_crear_bd_local():
-    db_user = os.getenv("DB_USER", "postgres")
-    db_pass = os.getenv("DB_PASSWORD", "1234")
-    db_host = os.getenv("DB_HOST", "localhost")
-    db_port = os.getenv("DB_PORT", "5432")
-    db_name = os.getenv("DB_NAME", "aps_local_db")
+    db_user = os.getenv("DB_USER")
+    db_pass = os.getenv("DB_PASSWORD")
+    db_host = os.getenv("DB_HOST")
+    db_port = os.getenv("DB_PORT")
+    db_name = os.getenv("DB_NAME")
 
     cadena_admin = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/postgres"
     engine_postgres = create_engine(cadena_admin, isolation_level="AUTOCOMMIT")
@@ -38,22 +38,22 @@ def get_engine(admin=False):
             return create_engine(db_url, pool_pre_ping=True)
             
         # Fallback a variables separadas (Evitando puertos nulos)
-        db_user = os.getenv("DB_USER_AIVEN", "")
-        db_pass = os.getenv("DB_PASSWORD_AIVEN", "")
-        db_host = os.getenv("DB_HOST_AIVEN", "")
-        db_port = os.getenv("DB_PORT_AIVEN") or "5432"  # Control de None
-        db_name = os.getenv("DB_NAME_AIVEN", "")
+        db_user = os.getenv("DB_USER_AIVEN")
+        db_pass = os.getenv("DB_PASSWORD_AIVEN")
+        db_host = os.getenv("DB_HOST_AIVEN")
+        db_port = os.getenv("DB_PORT_AIVEN")
+        db_name = os.getenv("DB_NAME_AIVEN")
         
         cadena = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}?sslmode=require&client_encoding=utf8"
         return create_engine(cadena, pool_pre_ping=True)
         
     else:
         # Entorno Local
-        db_user = os.getenv("DB_USER", "postgres")
-        db_pass = os.getenv("DB_PASSWORD", "1234")
-        db_host = os.getenv("DB_HOST", "localhost")
-        db_port = os.getenv("DB_PORT") or "5432"
-        db_name = "postgres" if admin else os.getenv("DB_NAME", "aps_local_db")
+        db_user = os.getenv("DB_USER")
+        db_pass = os.getenv("DB_PASSWORD")
+        db_host = os.getenv("DB_HOST")
+        db_port = os.getenv("DB_PORT")
+        db_name = "postgres" if admin else os.getenv("DB_NAME")
         
         cadena = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}?client_encoding=utf8"
         isolation = "AUTOCOMMIT" if admin else "READ COMMITTED"
